bbdfd814bf50feb96f683a802b671f569ff9f534
Justin Hamm <Justin.T.Hamm@gmail.com>
2026-08-11 19:24:18 -0700

    Added door colors and delay

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -3,18 +3,38 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] MeshRenderer doorRenderer;
+
+    [SerializeField] Color closedColor;
+    [SerializeField] Color openColor;
+    [SerializeField] Color delayColor;
+
+    [SerializeField] float openDelay = 1.0f;
+    float openTimer = 0.0f;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            animator.SetBool("Open", true);
+            openTimer = 0;
+            doorRenderer.material.color = delayColor;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            if (openTimer < openDelay)
+            {
+                openTimer += Time.deltaTime;
+            }
+            else
+            {
+                animator.SetBool("Open", true);
+                doorRenderer.material.color = openColor;
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -22,6 +42,7 @@ public class DoorTrigger : MonoBehaviour
         if (other.tag == "Player")
         {
             animator.SetBool("Open", false);
+            doorRenderer.material.color = closedColor;
         }
     }
 }
